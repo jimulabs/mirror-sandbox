3f57806a039490972e4bfa8af3de3d80c986cd0b
Linton Ye <linton@jimulabs.com>
2015-03-04 07:01:02 -0800

    Update comments

--- lib/src/main/java/com/jimulabs/mirrorsandbox/MirrorSandbox.java
+++ lib/src/main/java/com/jimulabs/mirrorsandbox/MirrorSandbox.java
@@ -20,10 +20,12 @@ import android.view.View;
 public interface MirrorSandbox {
     /**
      * Mirror calls this method from the UI thread right after the sandbox object is created, BEFORE
-     * the layout pass is done.
+     * the layout pass is done. This method can be used for sandbox-only initialization code.
      * <p/>
      * Methods with the prefix '$' are considered as "design mode" methods used for quick experiments
-     * or populating views with mock data. Do not call from production code.
+     * or populating views with mock data. Do not call from production code. However, it is encouraged
+     * to call other methods in the sandbox class from production code to reuse the code written in
+     * sandbox mode.
      *
      * @param rootView the root view that can be used to find child views in the layout
      */
@@ -34,7 +36,9 @@ public interface MirrorSandbox {
      * Methods such as View#getMeasuredWidth() will return proper values.
      * <p/>
      * Methods with the prefix '$' are considered as "design mode" methods used for quick experiments
-     * or populating views with mock data. Do not call from production code.
+     * or populating views with mock data. Do not call from production code. However, it is encouraged
+     * to call other methods in the sandbox class from production code to reuse the code written in
+     * sandbox mode.
      *
      * @param rootView the root view that can be used to find child views in the layout
      */
@@ -45,7 +49,9 @@ public interface MirrorSandbox {
      * This method can be used to release things that are not supposed to be persist across refreshes.
      * <p/>
      * Methods with the prefix '$' are considered as "design mode" methods used for quick experiments
-     * or populating views with mock data. Do not call from production code.
+     * or populating views with mock data. Do not call from production code. However, it is encouraged
+     * to call other methods in the sandbox class from production code to reuse the code written in
+     * sandbox mode.
      */
     void $onDestroy();
 }
