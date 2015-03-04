a3354115261f945488a98f79d0968489b5cc94df
Linton Ye <linton@jimulabs.com>
2015-03-04 07:27:18 -0800

    Remove <p/> from comments

--- lib/src/main/java/com/jimulabs/mirrorsandbox/MirrorSandbox.java
+++ lib/src/main/java/com/jimulabs/mirrorsandbox/MirrorSandbox.java
@@ -6,22 +6,22 @@ import android.view.View;
  * MirrorSandbox classes are considered "design mode" code for quick experiments or populating views
  * with mock data. Think of it as a REPL for Android UI (layouts and animations) when used in
  * combination with the Java code hot-swapping of Mirror.
- * <p/>
+ *
  * Mirror uses classes that implement this interface and set the "sandbox" attribute of
  * a mirror screen file:
- * <p/>
+ *
  * &lt;screen sandbox="foo.bar.BarBox"&gt;
- * <p/>
+ *
  * In most cases, you can subclass {@link com.jimulabs.mirrorsandbox.MirrorSandboxBase} which
  * provides some base implementations.
- * <p/>
+ *
  * Created by lintonye on 2014-12-23.
  */
 public interface MirrorSandbox {
     /**
      * Mirror calls this method from the UI thread right after the sandbox object is created, BEFORE
      * the layout pass is done. This method can be used for sandbox-only initialization code.
-     * <p/>
+     *
      * Methods with the prefix '$' are considered as "design mode" methods used for quick experiments
      * or populating views with mock data. Do not call from production code. However, it is encouraged
      * to call other methods in the sandbox class from production code to reuse the code written in
@@ -34,7 +34,7 @@ public interface MirrorSandbox {
     /**
      * Mirror calls this method from the UI thread after the layout pass for the entire screen finishes.
      * Methods such as View#getMeasuredWidth() will return proper values.
-     * <p/>
+     *
      * Methods with the prefix '$' are considered as "design mode" methods used for quick experiments
      * or populating views with mock data. Do not call from production code. However, it is encouraged
      * to call other methods in the sandbox class from production code to reuse the code written in
@@ -47,7 +47,7 @@ public interface MirrorSandbox {
     /**
      * Mirror calls this method from the UI thread in the Activity#onDestroy() call back.
      * This method can be used to release things that are not supposed to be persist across refreshes.
-     * <p/>
+     *
      * Methods with the prefix '$' are considered as "design mode" methods used for quick experiments
      * or populating views with mock data. Do not call from production code. However, it is encouraged
      * to call other methods in the sandbox class from production code to reuse the code written in
